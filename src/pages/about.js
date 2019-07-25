fb69d0ccacba213eb2943400e1eb69126d3adaaa
Alvaro <alvaro.serrano@wmich.edu>
2019-07-25 17:39:24 +0200

    created page for individual authors

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,8 +4,15 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const AboutPage = () => (
-  <Layout pageTitle="About us">
+  <Layout pageTitle="About">
     <SEO title="About" />
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eos culpa
+      dolorem nam veritatis tenetur, ducimus voluptate optio sapiente,
+      consequatur voluptatibus illum illo quisquam quidem molestiae officia,
+      aperiam provident doloremque.
+    </p>
   </Layout>
 )
 
