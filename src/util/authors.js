3896f56186b4aa74dbce9518032a6d386d25601e
Rivas <43758471+alvaroserrrano@users.noreply.github.com>
2019-08-06 10:11:56 -0500

    changes to my profile

--- src/util/authors.js
+++ src/util/authors.js
@@ -23,14 +23,14 @@ const authors = [
   },
   {
     name: "Alvaro Serrano",
-    imageUrl: "jane.jpg",
+    imageUrl: "me.jpg",
     bio:
       "Alvaro is a full-stack developer, he specializes in security and her favourite stack is the MERN stack",
     facebook: "https://www.facebook.com/",
     twitter: "https://www.twitter.com/",
     instagram: "https://www.instagram.com/",
     google: "https://www.google.com/",
-    linkedin: "https://www.linkedin.com/",
+    linkedin: "https://www.linkedin.com/in/alvaro-serrano-rivas",
   },
 ]
 
