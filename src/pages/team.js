7f14d727f5993987584bf7e379a9b4eac15f480b
Alvaro <alvaro.serrano@wmich.edu>
2019-12-27 11:17:37 +0100

    removed useless authors

--- src/pages/team.js
+++ src/pages/team.js
@@ -13,7 +13,11 @@ const TeamPage = () => (
     <SEO title="Our Team" keywords={[`gatsby`, `application`, `react`]} />
     <Row className="mb-4">
       <div className="col-md-3">
-        <img src={JohnImage} style={{ maxWidth: "100%" }} alt="John profile" />
+        <img
+          src={AlvaroImage}
+          style={{ maxWidth: "100%" }}
+          alt="Alvaro profile"
+        />
       </div>
       <div className="col-md-8">
         <Card style={{ minHeight: "100%", backgroundColor: "#353a3f" }}>
@@ -26,54 +30,6 @@ const TeamPage = () => (
               classname="text-uppercase"
               color="primary"
               href={`/author/${slugify(authors[0].name)}`}
-            >
-              View posts
-            </Button>
-          </CardBody>
-        </Card>
-      </div>
-    </Row>
-    <Row className="mb-4">
-      <div className="col-md-3">
-        <img src={JaneImage} style={{ maxWidth: "100%" }} alt="Jane profile" />
-      </div>
-      <div className="col-md-8">
-        <Card style={{ minHeight: "100%", backgroundColor: "#353a3f" }}>
-          <CardBody>
-            <CardTitle style={{ color: "#41FF00" }}>
-              {authors[1].name}
-            </CardTitle>
-            <CardText>{authors[1].bio}</CardText>
-            <Button
-              classname="text-uppercase"
-              color="primary"
-              href={`/author/${slugify(authors[1].name)}`}
-            >
-              View posts
-            </Button>
-          </CardBody>
-        </Card>
-      </div>
-    </Row>
-    <Row className="mb-4">
-      <div className="col-md-3">
-        <img
-          src={AlvaroImage}
-          style={{ maxWidth: "100%" }}
-          alt="Alvaro profile"
-        />
-      </div>
-      <div className="col-md-8">
-        <Card style={{ minHeight: "100%", backgroundColor: "#353a3f" }}>
-          <CardBody>
-            <CardTitle style={{ color: "#41FF00" }}>
-              {authors[1].name}
-            </CardTitle>
-            <CardText>{authors[1].bio}</CardText>
-            <Button
-              classname="text-uppercase"
-              color="primary"
-              href={`/author/${slugify(authors[1].name)}`}
             >
               View posts
             </Button>
