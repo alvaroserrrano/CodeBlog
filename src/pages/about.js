34fc2993d8427c34c60bbfdba19464bcb2cd7e50
Alvaro <alvaro.serrano@wmich.edu>
2019-12-27 11:28:38 +0100

    about changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,6 +12,22 @@ const AboutPage = () => (
       posting about IT, ethical hacking, web development, databases, systems
       administration, programming practices... and much more!
     </p>
+    <br />
+    <p>
+      If you want to learn more about my skills you can check out my temporary
+      portfolio site:{" "}
+      <a href="https://alvaro-serrano-rivas.netlify.com" target="_blank">
+        Alvaro Serrano
+      </a>
+    </p>
+    <br />
+    <p>
+      And also my Github Profile:{" "}
+      <a href="https://github.com/alvaroserrrano" target="_blank">
+        {" "}
+        Alvaro Serrano
+      </a>
+    </p>
   </Layout>
 )
 
